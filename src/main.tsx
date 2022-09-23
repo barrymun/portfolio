fc7a50da3fcc631dd772ca4f3d4402b8b2810606
Neil <barrymun@tcd.ie>
2022-09-23 11:34:43 +0100

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from './pages/App'
+import Error from './pages/Error'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <RouterProvider router={router} />
 )
