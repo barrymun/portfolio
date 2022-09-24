4b037a7cc8243a90c1165d4858dcc613629cda79
Neil <barrymun@tcd.ie>
2022-09-24 17:04:29 +0100

    remove onclick logic

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -159,10 +159,6 @@ function Resume() {
     },
   ];
 
-  const onClick = (e: React.MouseEvent<Element, MouseEvent>) => {
-    console.log('HERE')
-  }
-
   return (
     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
@@ -203,7 +199,7 @@ function Resume() {
         ))}
       </ol>
       <a href={resumePDF} target={URI_TARGET} rel={URI_REL} download>
-        <Button variant="secondary" className="group mt-6 w-full" onClick={onClick}>
+        <Button variant="secondary" className="group mt-6 w-full">
           Download CV
           <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
         </Button>
