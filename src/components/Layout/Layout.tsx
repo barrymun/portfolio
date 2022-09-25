4e5f3c615fa6f98545d49191c1ba0971612c3a0b
Neil <barrymun@tcd.ie>
2022-09-25 18:41:12 +0100

    working dark mode

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 export default function Layout() {
   return (
     <>
-      <div className="bg-zinc-50 fixed flex justify-center sm:px-8 inset-0">
+      <div className="bg-zinc-50 dark:bg-black fixed flex justify-center sm:px-8 inset-0">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
         </div>
