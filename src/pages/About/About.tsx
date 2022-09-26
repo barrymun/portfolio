c3b1f363140cc519602913aac6a5fdcaea927916
Neil <barrymun@tcd.ie>
2022-09-26 12:42:00 +0100

    about page content

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,8 @@
 import clsx from 'clsx'
-import { Link } from 'react-router-dom';
+import imagePilot from '@/assets/pilot.jpeg'
 import Container from '@/components/Container';
 import GitHubIcon from "@/components/SocialIcons/GitHubIcon";
 import LinkedInIcon from "@/components/SocialIcons/LinkedInIcon";
-import portraitImage from '@/images/portrait.jpg'
 import { URI_TARGET, URI_REL, GITHUB_PROFILE_URI, LINKEDIN_PROFILE_URI } from '@/constants';
 
 function SocialLink({ className, href, children, icon: Icon }) {
@@ -50,7 +49,7 @@ export default function About() {
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
               <img
-                src={portraitImage}
+                src={imagePilot}
                 alt=""
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
               />
@@ -58,35 +57,19 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+              Hi I’m Neil.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I'm a full stack engineer. I like to design and build things,
+                and I have a decent knowledge of many different technologies.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                Co-founded a VC funded technology company in 2017.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
-              </p>
-              <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Being an aviation enthusiast I'm a pilot, and I also enjoy flying drones.
+                I also enjoy swimming, running and listening to music.
               </p>
             </div>
           </div>
