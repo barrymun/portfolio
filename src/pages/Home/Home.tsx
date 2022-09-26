68b952497f5efe910e87f59a4dcc9e6f8f0dbba9
Neil <barrymun@tcd.ie>
2022-09-26 12:49:40 +0100

    cleanup

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,5 @@
 import React, { FC, useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
 import clsx from "clsx";
-import { formatDate } from "@/lib/formatDate";
-import Card from "@/components/Card";
 import GitHubIcon from "@/components/SocialIcons/GitHubIcon";
 import LinkedInIcon from "@/components/SocialIcons/LinkedInIcon";
 import Button from "@/components/Button";
@@ -20,8 +17,8 @@ import { GITHUB_PROFILE_URI, LINKEDIN_PROFILE_URI, URI_REL, URI_TARGET } from "@
 
 const IMAGES = [image1, image2, image3, image4, image5];
 const IMAGE_ROTATIONS = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2'];
-const CANVAS_WIDTH = window.innerWidth;
-const SCROLL_SPEED = 4.3;
+// const CANVAS_WIDTH = window.innerWidth;
+// const SCROLL_SPEED = 4.3;
 
 // function MailIcon(props: any) {
 //   return (
@@ -101,35 +98,6 @@ function SocialLink({
     </button>
   );
 }
-
-// function Newsletter() {
-//   return (
-//     <form
-//       action="/thank-you"
-//       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-//     >
-//       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-//         <MailIcon className="h-6 w-6 flex-none" />
-//         <span className="ml-3">Stay up to date</span>
-//       </h2>
-//       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-//         Get notified when I publish something new, and unsubscribe at any time.
-//       </p>
-//       <div className="mt-6 flex">
-//         <input
-//           type="email"
-//           placeholder="Email address"
-//           aria-label="Email address"
-//           required
-//           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
-//         />
-//         <Button type="submit" className="ml-4 flex-none">
-//           Join
-//         </Button>
-//       </div>
-//     </form>
-//   );
-// }
 
 function Resume() {
   let resume = [
@@ -300,19 +268,6 @@ export default function Home() {
         <Resume />
       </Container>
 
-      {/* <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
-          </div>
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-            <Newsletter />
-            <Resume />
-          </div>
-        </div>
-      </Container> */}
     </ >
   );
 }
